f21db1897f4e4ad28657ed0fe8ebeb00e570eac2
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2023-02-05 13:02:27 +0100

    +button

--- UIKit-Extension/ViewControllers/Buttons/ButtonViewController.swift
+++ UIKit-Extension/ViewControllers/Buttons/ButtonViewController.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import UIKit
+import SwiftUIJo
+
+class ButtonViewController: UIViewController {
+    
+    //MARK: - UI Propreties
+
+    //MARK: - Variables
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray5
+    }
+    
+    //MARK: - Init
+    
+    //MARK: - UI Setupsr
+    private func setupSubViews() {
+        
+    }
+    
+    private func setupConstraints() {
+        
+    }
+    
+    //MARK: - Functions
+    
+    //MARK: - Extensions
+    
+}
