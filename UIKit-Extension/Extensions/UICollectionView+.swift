4e321bc7914d601f29cd2bdcadf1f6b8f30bb503
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2023-02-06 13:05:34 +0100

    +extension

--- UIKit-Extension/Extensions/UICollectionView+.swift
+++ UIKit-Extension/Extensions/UICollectionView+.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import UIKit
+
+extension UICollectionViewFlowLayout {
+    
+    func exploreLayout() -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width/2 - 15, height: UIScreen.main.bounds.width/2 - 15)
+        layout.minimumLineSpacing = 5
+        layout.minimumInteritemSpacing = 5
+        layout.scrollDirection = .vertical
+        return layout
+    }
+    
+}
+
+extension UICollectionView {
+    
+    public func reuseIdentifier<T>(for type: T.Type) -> String {
+        return String(describing: type)
+    }
+    
+    public func dequeResuableCell<T: UICollectionViewCell>(for type: T.Type, for indexPath: IndexPath) -> T {
+        guard let cell = dequeueReusableCell(withReuseIdentifier: reuseIdentifier(for: type), for: indexPath) as? T else {
+            fatalError("failed to deque cell.")
+        }
+        return cell
+    }
+    
+    public func registerCell<T: UICollectionViewCell>(_ type: T.Type) {
+        register(type, forCellWithReuseIdentifier: reuseIdentifier(for: type))
+    }
+    
+}
