f569110df289bfe514bfff2ab3a26b88b750c7fd
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2023-02-08 13:07:54 +0100

    +Commit

--- UIKit-Extension/ViewControllers/Main/Views/UIElementCell.swift
+++ UIKit-Extension/ViewControllers/Main/Views/UIElementCell.swift
@@ -9,4 +9,49 @@ import UIKit
 
 class UIElementCell: UICollectionViewCell {
     
+    lazy var iconImage: UIImageView = {
+        let img = UIImageView()
+        img.translatesAutoresizingMaskIntoConstraints = false
+        tintColor = .darkGray
+        return img
+    }()
+    
+    lazy var name: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.textColor = .darkGray
+        return lbl
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+        layer.cornerRadius = 10
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup() {
+        addSubview(iconImage)
+        addSubview(name)
+        let constraints = [
+            iconImage.centerXAnchor.constraint(equalTo: centerXAnchor),
+            iconImage.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -30),
+            iconImage.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.3),
+            iconImage.heightAnchor.constraint(equalTo: iconImage.widthAnchor),
+            
+            name.centerXAnchor.constraint(equalTo: centerXAnchor),
+            name.topAnchor.constraint(equalTo: iconImage.bottomAnchor, constant: 30)
+        ]
+        NSLayoutConstraint.activate(constraints)
+    }
+    
+    public func configure(element: UIElement) {
+        self.iconImage.image = element.image
+        self.name.text = element.name
+    }
+    
 }
